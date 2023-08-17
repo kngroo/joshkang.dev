3e093317807c4241f4451ef385c0434e924995e0
Josh Kang <3375278+kngroo@users.noreply.github.com>
2023-08-17 18:10:32 +0900

    Add maintenance route and redirect

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { DotLottiePlayer, PlayerEvents } from "@dotlottie/react-player";
+
+export default function Maintenance() {
+  const [isReady, setIsReady] = useState(false);
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <DotLottiePlayer
+        className="lg:w-1/2 md:w-3/4 w-full"
+        autoplay={true}
+        loop={true}
+        src="/under-maintenance.lottie"
+        onEvent={(event: PlayerEvents) => {
+          if (event === PlayerEvents.Ready) {
+            setIsReady(true);
+          }
+        }}
+      />
+      {isReady && (
+        <div className="text-center">
+          <h1 className="text-lg">Under maintenance</h1>
+          <p className="text-sm">Be back soon!</p>
+        </div>
+      )}
+    </main>
+  );
+}
