69c6cd749f7de5fcb42461af81d83c2fa8a17905
Josh Kang <3375278+kngroo@users.noreply.github.com>
2023-08-17 17:32:22 +0900

    Replace dotlottie player and use static lottie

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
 "use client";
 
-import { useRef } from "react";
-import "@johanaarstein/dotlottie-player";
-import type { DotLottiePlayer } from "@johanaarstein/dotlottie-player";
+import { useState } from "react";
+import { DotLottiePlayer, PlayerEvents } from "@dotlottie/react-player";
 
 export default function Home() {
-  const animation = useRef<DotLottiePlayer | null>(null);
+  const [isReady, setIsReady] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <dotlottie-player
-        autoplay
-        loop
-        ref={animation}
-        src="https://lottie.host/5ac30703-4e56-4c23-a52f-7f2c93bf88df/9wWOguPj9f.lottie"
+    <main className="flex min-h-screen flex-col items-center lg:p-24">
+      <DotLottiePlayer
+        autoplay={true}
+        loop={true}
+        src="/under-maintenance.lottie"
+        onEvent={(event: PlayerEvents) => {
+          if (event === PlayerEvents.Ready) {
+            setIsReady(true);
+          }
+        }}
       />
-      <div className="text-center">
-        <h1 className="text-lg">Under maintenance</h1>
-        <p className="text-sm">Be back soon!</p>
-      </div>
+      {isReady && (
+        <div className="text-center">
+          <h1 className="text-lg">Under maintenance</h1>
+          <p className="text-sm">Be back soon!</p>
+        </div>
+      )}
     </main>
   );
 }
